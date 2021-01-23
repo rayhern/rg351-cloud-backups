7a4ba01304180c6950768d42596fb8e216efbcf5
rayhern <bizong@gmail.com>
2021-01-22 21:23:40 -0800

    more sync save cleanup.

--- syncsaves.py
+++ syncsaves.py
@@ -28,7 +28,7 @@ REMOTE_GBA_PATH = config['remote']['GBA_SAVE_PATH']
 REMOTE_GBC_PATH = config['remote']['GBC_SAVE_PATH']
 REMOTE_GB_PATH = config['remote']['GB_SAVE_PATH']
 REMOTE_PSX_PATH= config['remote']['PSX_SAVE_PATH']
-REMOTE_DIABLO_PATH = config['remote']['DIABLO_PATH']
+REMOTE_DIABLO_PATH = config['remote']['DIABLO_SAVE_PATH']
 
 DEVILUTION_SAVE_PATH = config['local']['DEVILUTION_SAVE_PATH']
 LOCAL_PSP_PATH = config['local']['PSP_SAVE_PATH']
@@ -54,10 +54,18 @@ def main():
     print("    WARNING: Edit config.ini before running this   ")
     print("Backup save files from which location?")
     print("------------------------------------------")
+    print("1.) Backup RG350/RG351/Raspberry Pi Saved Games")
+    print("2.) Backup local save games.")
     print("")
-    print("Press ENTER if you are ready to backup to the cloud.")
-    raw_input = input()
-    os.system('clear')
+    selection = int(input("Enter your selection: ").strip())
+    print("")
+    if selection == "":
+        print("You must enter a valid selection.")
+        sys.exit(0)
+    if (selection == 1 or selection == 2):
+        sync_saves(selection)
+    else:
+        print("Does not compute.")
 
 def send_rsync(source, destination):
     global SSH_USER, SSH_PASSWORD, SSH_IP_ADDRESS
@@ -77,26 +85,24 @@ def send_rsync3(source, destination, extension):
         SSH_PASSWORD, extension, SSH_USER, SSH_IP_ADDRESS, source, destination
     ))
     
-def sync_saves():
-
-    # Check to see if any previous diablo saves exist.
-    mac_diablo_dir = TEMP_SAVE_PATH + '/diablo'
+def sync_saves(answer):
 
     # TODO : Compare sizes and give warnings if smaller than whats getting replaced.
     # User has chosen to get all of the save games from the device, and udpate the cloud.
-    if os.path.exists(mac_diablo_dir):
-        print('Removing old diablo save files from: %s...' % mac_diablo_dir);
-        os.system('chmod +rw %s' % mac_diablo_dir)
-        shutil.rmtree(mac_diablo_dir)
-        os.mkdir(mac_diablo_dir)
+    if answer == 1:
+
+        if os.path.exists(TEMP_SAVE_PATH + "/diablo"):
+            print('Removing old diablo save files from: %s...' % TEMP_SAVE_PATH + "/diablo");
+            os.system('chmod +rw %s' % TEMP_SAVE_PATH + "/diablo")
+            shutil.rmtree(TEMP_SAVE_PATH + "/diablo")
+            os.mkdir(TEMP_SAVE_PATH + "/diablo")
         
         # Copy all of diablo/devilutionX files. Also save the save files for local play.
-        mac_diablo_dir = TEMP_SAVE_PATH + '/diablo'
         print("Copying Diablo save files from remote %s..." % REMOTE_DIABLO_PATH)
-        send_rsync2(REMOTE_DIABLO_PATH, mac_diablo_dir, 'sv')
+        send_rsync2(REMOTE_DIABLO_PATH, TEMP_SAVE_PATH + "/diablo", 'sv')
         # Copy updated save files to local diablo directory.
         print("Copying save files to local devilution directory...")
-        os.system("cp -R %s %s" % (mac_diablo_dir + '/', DEVILUTION_SAVE_PATH))
+        os.system("cp -R %s %s" % (TEMP_SAVE_PATH + "/diablo/", DEVILUTION_SAVE_PATH))
 
         # Copying all PSP save files.
         print("Copying PSP save files from device...")
@@ -125,12 +131,37 @@ def sync_saves():
                 os.system('mkdir %s' % LOCAL_GOOGLE_DRIVE_PATH)
 
             print("Copying all save files to Google Drive...")
-            os.system("rsync -a %s %s" % (mac_diablo_dir + '/', LOCAL_GOOGLE_DRIVE_PATH + '/diablo'))
+            os.system("rsync -a %s %s" % (TEMP_SAVE_PATH + '/diablo/', LOCAL_GOOGLE_DRIVE_PATH + '/diablo'))
             os.system("rsync -a %s %s" % (TEMP_SAVE_PATH + '/gba/', LOCAL_GOOGLE_DRIVE_PATH + '/gba'))
             os.system("rsync -a %s %s" % (TEMP_SAVE_PATH + '/gbc/', LOCAL_GOOGLE_DRIVE_PATH + '/gbc'))
             os.system("rsync -a %s %s" % (TEMP_SAVE_PATH + '/gb/', LOCAL_GOOGLE_DRIVE_PATH + '/gb'))
             os.system("rsync -a %s %s" % (TEMP_SAVE_PATH + '/psp/', LOCAL_GOOGLE_DRIVE_PATH + '/psp'))
             os.system("rsync -a %s %s" % (TEMP_SAVE_PATH + '/psx/', LOCAL_GOOGLE_DRIVE_PATH + '/psx'))
+
+    elif answer == 2:
+        # The user has chosen to send saves from local pc to handheld and google clould.
+        if os.path.exists(TEMP_SAVE_PATH + "/diablo"):
+            print('Removing old diablo save files from: %s...' % TEMP_SAVE_PATH + "/diablo");
+            os.system('chmod +rw %s' % TEMP_SAVE_PATH + "/diablo")
+            shutil.rmtree(TEMP_SAVE_PATH + "/diablo")
+            os.mkdir(TEMP_SAVE_PATH + "/diablo")
+        
+        if (os.path.exists(TEMP_SAVE_PATH) is False):
+            os.mkdir(TEMP_SAVE_PATH)
+        
+        print("Preparing PSP saves to send...")
+        os.system("rsync -a %s %s" % (LOCAL_PSP_PATH, TEMP_SAVE_PATH + '/psp'))
+        print("Preparing devilutionX save files to send...")
+        os.system('rsync -a %s/*.sv %s' % (DEVILUTION_SAVE_PATH, TEMP_SAVE_PATH + '/diablo'))
+
+        if LOCAL_GOOGLE_DRIVE_PATH != "":
+
+            if os.path.exists(LOCAL_GOOGLE_DRIVE_PATH) is False:
+                os.system('mkdir %s' % LOCAL_GOOGLE_DRIVE_PATH)
+
+            print("Copying all save files to Google Drive...")
+            os.system("rsync -a %s %s" % (TEMP_SAVE_PATH + '/diablo/', LOCAL_GOOGLE_DRIVE_PATH + "/diablo"))
+            os.system("rsync -a %s %s" % (TEMP_SAVE_PATH + '/psp/', LOCAL_GOOGLE_DRIVE_PATH + "/psp"))
 
 
 if __name__ == "__main__":
